fafaf47dfee018822e8ed72bd6e3d22bc1d36849
Caroline Eubanks <cmeubanks@gmail.com>
2020-12-29 14:24:18 -0700

    completed test through Lift-test #5

--- src/Lift.js
+++ src/Lift.js
@@ -1,7 +1,16 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(number){
+  this.inService = true;
+  this.limit = number;
+  this.skiers = [];
+  this.safetyBar = 'up';
+}
+admitSkier(name, boolean) {
+//this.skiers.push(1);
+this.skiers.push({name: name});
+}
 }
 
 module.exports = Lift;
