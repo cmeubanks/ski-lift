ce1e576f1c0e825a43c4cacb05c61f7291b958c0
Caroline Eubanks <cmeubanks@gmail.com>
2020-12-29 12:32:02 -0700

    first commit

--- src/Skier.js
+++ src/Skier.js
@@ -1,5 +1,20 @@
 class Skier {
-
+  constructor(nameValue, boolean){
+  this.name = nameValue;
+  this.hasLiftTicket = boolean;
+  this.skillLevel = 1;
+  this.nextSlope = 'green circle';
+}
+takeLesson(){
+this.skillLevel++
+}
+pickSlope() {
+  if(this.skillLevel <= 4){
+  this.nextSlope = 'blue square';
+} else {
+  this.nextSlope = 'black diamond';
+}
+}
 }
 
 module.exports = Skier;
